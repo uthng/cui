f33fa9abfce356d4a05e0666475c32ad20b0f806
uthng <u.nguyenthanh@gmail.com>
2018-03-26 01:39:51 +0200

    - Add badge modifications & button apply

--- pages/kv.vue
+++ pages/kv.vue
@@ -1,17 +1,21 @@
 <template>
-  <div>
-    <tree-view :data="jsonSource" :options="{maxDepth: 7}"></tree-view>
-    <v-dialog v-model="dialog2" max-width="500px">
-      <v-card>
-        <v-card-title>
-          Dialog 2
-        </v-card-title>
-        <v-card-actions>
-          <v-btn color="primary" flat @click.stop="dialog2=false">Close</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </div>
+  <v-container fluide>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <div class="text-xs-right mb-4">
+          <v-badge left class="mx-0 px-0">
+            <span slot="badge">{{ nb_modifications }}</span>
+            <span class="grey--text text--darken-1">MODIFICATIONS</span>
+          </v-badge>
+          &nbsp;&nbsp;
+          <v-btn flat disabled class="mx-0 px-0">Apply</v-btn>
+        </div>
+      </v-flex>
+      <v-flex xs12>
+        <tree-view :data="jsonSource" :options="{maxDepth: 7}"></tree-view>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -23,7 +27,6 @@ export default {
   data: () => {
   return {
   //    jsonSource: {"csl-tec-vault-dc1-fr":{"auth":{"959668e3-af15-d83e-c6d9-99df1ff57a31":{"group":{},"user":{}}},"core":{"cluster":{"local":{}},"leader":{},"wrapping":{}},"logical":{"2565dd12-8b0f-3736-9f2d-3ea9d5a6cc27":{"chrysalis":{"apikey":{"phpipam":{}},"passwords":{"powerdns":{}},"ssh-keys":{}},"partners":{"ssh-keys":{}},"travel":{"ssh-keys":{}}},"784394ed-7dfb-1df6-a6c1-1394ff26cd26":{"packer":{"buckets":{}}}},"sys":{"policy":{},"token":{"accessor":{},"id":{}}}}}
-      dialog2: false
     }
   },
   components: {
