3c6b67735d39b6599d95fa53a0955758027d9103
uthng <u.nguyenthanh@gmail.com>
2018-04-04 00:26:21 +0200

    - Add async/await for the request getting acl list terminates completely

--- middleware/check-acl.js
+++ middleware/check-acl.js
@@ -1,3 +1,7 @@
-export default function ({ store }) {
-  store.dispatch('checkConsulAcl')
+// Add async & await to wait axios request terminate completely
+// if not, computed value isAclEnabled in page rendering will take
+// the default value of consulAcl defined in store before
+// the request is promised
+export default async function ({ store }) {
+  await store.dispatch('checkConsulAcl')
 }
